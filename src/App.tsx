0d56ccadd5a5f1bb0f114cf3e769ffe096979bf3
Abdelrahman Mostafa <112385425+Abdouumostafa@users.noreply.github.com>
2024-02-01 22:21:46 +0200

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
-import { Route, RouterProvider, Routes, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
 import { pathList } from "./routes/routesPaths"
 import Login from "./pages/auth/Login"
+import ForgetPassword from "./pages/auth/ForgetPassword"
+import ForgetPasswordOTP from "./pages/auth/ForgetPasswordOTP"
+import ResetPassword from "./pages/auth/ResetPassword"
 
 function App() {
 
-  // const router = createBrowserRouter(
-  //   createRoutesFromElements(
-  //     <>
-
-  //     </>
-  //   )
-  // )
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path={pathList.login}>
+          <Route element={<Login />} index />
+          <Route path={pathList.forgetPassword}>
+            <Route element={<ForgetPassword />} index />
+            <Route element={<ForgetPasswordOTP />} path={pathList.forgetPasswordOTP} />
+            <Route element={<ResetPassword />} path={pathList.forgetPassword} />
+          </Route>
+        </Route>
+      </>
+    )
+  )
   return (
-    // <RouterProvider router={router} />
-    <Login />
+    <RouterProvider router={router} />
   )
 }
 
--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,4 +1,4 @@
-import AuthLayout from "@/layouts/AuthLayout"
+import AuthLayout from "../../layouts/AuthLayout"
 
 const Login = () => {
     return (
