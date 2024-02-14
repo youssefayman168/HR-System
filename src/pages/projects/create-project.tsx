8899e63a3530cc15fb493cf49e06855096e4bf17
steven <stevensary995@gmail.com>
2024-02-14 15:27:29 +0200

    EditDateInp

--- src/pages/projects/create-project.tsx
+++ src/pages/projects/create-project.tsx
@@ -2,9 +2,28 @@ import { BiChevronRight } from "react-icons/bi";
 import BaseLayout from "@/layouts/BaseLayout/BaseLayout";
 import { pathList } from "@/routes/routesPaths";
 import { Link } from "react-router-dom";
-import BaseInput from "@/components/BaseInput";
+import { useRef, useState } from "react";
 
 const CreateProject = () => {
+
+const dateInpF = useRef<HTMLInputElement>(null);
+const [dateF, setDateF] = useState<string>('From')
+
+  const dataInputFromOnChange = () => {
+    if (dateInpF.current) {
+      setDateF(dateInpF.current.value);
+    }
+  };
+
+const dateInpT = useRef<HTMLInputElement>(null);
+const [dateT, setDateT] = useState<string>('To')
+
+  const dataInputToOnChange = () => {
+    if (dateInpT.current) {
+      setDateT(dateInpT.current.value);
+    }
+  };
+
   return (
     <BaseLayout>
       <div className="p-5">
@@ -55,18 +74,18 @@ const CreateProject = () => {
                   <div className="flex items-center gap-4 w-fit ">
 
                   <div className="relative ">
-                    <input className="border-2 px-[17.6px] rounded-[10px] absolute inset-0 z-[99] opacity-0 " type="date"  />
+                    <input ref={dateInpF} onChange={dataInputFromOnChange} className="border-2 px-[17.6px] rounded-[10px] absolute inset-0 z-[99] opacity-0 " type="date"  />
                     <div className="border-2 p-4 rounded-[10px] flex items-center gap-44 ">
-                      <p>From</p>
+                      <p>{dateF}</p>
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M22 14V12C22 11.161 22 10.4153 21.9871 9.75H2.0129C2 10.4153 2 11.161 2 12V14C2 17.7712 2 19.6569 3.17157 20.8284C4.34315 22 6.22876 22 10 22H14C17.7712 22 19.6569 22 20.8284 20.8284C22 19.6569 22 17.7712 22 14Z" fill="#626262"/><path d="M7.75 2.5C7.75 2.08579 7.41421 1.75 7 1.75C6.58579 1.75 6.25 2.08579 6.25 2.5V4.07926C4.81067 4.19451 3.86577 4.47737 3.17157 5.17157C2.47737 5.86577 2.19451 6.81067 2.07926 8.25H21.9207C21.8055 6.81067 21.5226 5.86577 20.8284 5.17157C20.1342 4.47737 19.1893 4.19451 17.75 4.07926V2.5C17.75 2.08579 17.4142 1.75 17 1.75C16.5858 1.75 16.25 2.08579 16.25 2.5V4.0129C15.5847 4 14.839 4 14 4H10C9.16097 4 8.41527 4 7.75 4.0129V2.5Z" fill="#626262"/></svg>
 
                     </div>
                   </div>
 
                   <div className="relative">
-                    <input className="border-2 px-[17.6px] rounded-[10px] absolute inset-0 z-[99] opacity-0 " type="date"  />
+                    <input onChange={dataInputToOnChange} ref={dateInpT} className="border-2 px-[17.6px] rounded-[10px] absolute inset-0 z-[99] opacity-0 " type="date"  />
                     <div className="border-2 p-4 rounded-[10px] flex items-center gap-44 ">
-                      <p>To</p>
+                      <p>{dateT}</p>
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M22 14V12C22 11.161 22 10.4153 21.9871 9.75H2.0129C2 10.4153 2 11.161 2 12V14C2 17.7712 2 19.6569 3.17157 20.8284C4.34315 22 6.22876 22 10 22H14C17.7712 22 19.6569 22 20.8284 20.8284C22 19.6569 22 17.7712 22 14Z" fill="#626262"/><path d="M7.75 2.5C7.75 2.08579 7.41421 1.75 7 1.75C6.58579 1.75 6.25 2.08579 6.25 2.5V4.07926C4.81067 4.19451 3.86577 4.47737 3.17157 5.17157C2.47737 5.86577 2.19451 6.81067 2.07926 8.25H21.9207C21.8055 6.81067 21.5226 5.86577 20.8284 5.17157C20.1342 4.47737 19.1893 4.19451 17.75 4.07926V2.5C17.75 2.08579 17.4142 1.75 17 1.75C16.5858 1.75 16.25 2.08579 16.25 2.5V4.0129C15.5847 4 14.839 4 14 4H10C9.16097 4 8.41527 4 7.75 4.0129V2.5Z" fill="#626262"/></svg>
 
                     </div>
