2ef1ae4c43906e171da5b0862a313addb9ef3c2f
steven <stevensary995@gmail.com>
2024-02-17 19:29:37 +0200

    .

--- src/components/Table/ProjectsTable.tsx
+++ src/components/Table/ProjectsTable.tsx
@@ -1,7 +1,23 @@
 import Element from './Element'
 import DropDown from './DropDown'
+import { useEffect, useRef, useState } from 'react'
 
 const ProjectsTable = () => {
+
+    const ulStatusDrop = useRef<HTMLUListElement>(null)
+    const [ dropDown , setDropDown ] = useState(false)
+    console.log(dropDown)
+    useEffect(() =>{
+        if(dropDown) {
+            ulStatusDrop.current.style.height = "254px"
+        }else{
+            // @ts-ignore
+            ulStatusDrop.current.style.height = "0"
+        }
+    },[])
+
+
+
   return (
     <div className='bg-white px-10 rounded-[20px] '>
 
@@ -15,12 +31,13 @@ const ProjectsTable = () => {
                     <th>Starting Date</th>
                     <th>Expiry Date</th>
                     <th className='statusDropDown cursor-pointer relative'>
-                        <div className='flex items-center justify-center gap-2'>
+                        <div onClick={() => setDropDown(!dropDown) } className='flex items-center justify-center gap-2'>
                             Status
                             <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.2778 11.8764L14.1001 6.9035C14.401 6.5932 14.2186 6 13.8223 6H4.17773C3.78142 6 3.59902 6.5932 3.89992 6.9035L8.72219 11.8764C8.88205 12.0412 9.11795 12.0412 9.2778 11.8764Z" fill="#8F93AC"/></svg>
                         </div>
-
-                        <DropDown text1='All' text2='Planning' text3='In progress' text4='Done' text5='Canceled' />
+                        <ul ref={ulStatusDrop} className='DropDownTable duration-300 overflow-hidden w-[225px] text-start bg-white shadow-xl rounded-[10px] border-[1px] border-[#8F93AC] absolute left-[20px] top-[70px] h-0 ' >
+                            <DropDown text1='All' text2='Planning' text3='In progress' text4='Done' text5='Canceled' />
+                        </ul>
                     </th>
                     <th>Project Hours</th>
                     <th>Work Hours</th>
