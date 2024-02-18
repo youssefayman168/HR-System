052b780a833c6fe607daffd2be06362e89096daa
steven <stevensary995@gmail.com>
2024-02-18 13:57:16 +0200

    .

--- src/components/Table/ProjectsTable.tsx
+++ src/components/Table/ProjectsTable.tsx
@@ -1,5 +1,6 @@
 import Element from './Element'
 import DropDown from './DropDown'
+import DropIcon from '../../assets/Projects/DropDown.svg'
 import { useState } from 'react'
 
 const ProjectsTable = () => {
@@ -19,7 +20,7 @@ const ProjectsTable = () => {
                         <th className='statusDropDown cursor-pointer relative'>
                             <div className='flex items-center justify-center gap-2' onClick={() => setOpenList(!openList)}>
                                 Status
-                                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9.2778 11.8764L14.1001 6.9035C14.401 6.5932 14.2186 6 13.8223 6H4.17773C3.78142 6 3.59902 6.5932 3.89992 6.9035L8.72219 11.8764C8.88205 12.0412 9.11795 12.0412 9.2778 11.8764Z" fill="#8F93AC" /></svg>
+                                <img src={DropIcon} alt="DropIcon" />
                             </div>
 
                             <DropDown style={{ height: openList ? '254px' : '0', opacity: openList ? '1' : '0' }} text1='All' text2='Planning' text3='In progress' text4='Done' text5='Canceled' />
@@ -46,6 +47,18 @@ const ProjectsTable = () => {
                     </tr>
                     <tr className='border-b-[2px]' >
                         <Element id='2341421' projects='Architectural drawing' company='Smart Engineering Consultancy' startingDate='29 July 2023' expiryDate='29 July 2023' status='Canceled' projectHours='200h' workHours='10h 2m' styles={{ color: "#AA0000", backgroundColor: "#FFE3E3" }} />
+                    </tr>
+                    <tr>
+                        <Element id='2341421' projects='Architectural drawing' company='Smart Engineering Consultancy' startingDate='29 July 2023' expiryDate='29 July 2023' status='Done' projectHours='200h' workHours='10h 2m' styles={{ color: "#34E045", backgroundColor: "#DEFFE1" }} />
+                    </tr>
+                    <tr>
+                        <Element id='2341421' projects='Architectural drawing' company='Smart Engineering Consultancy' startingDate='29 July 2023' expiryDate='29 July 2023' status='Done' projectHours='200h' workHours='10h 2m' styles={{ color: "#34E045", backgroundColor: "#DEFFE1" }} />
+                    </tr>
+                    <tr>
+                        <Element id='2341421' projects='Architectural drawing' company='Smart Engineering Consultancy' startingDate='29 July 2023' expiryDate='29 July 2023' status='Done' projectHours='200h' workHours='10h 2m' styles={{ color: "#34E045", backgroundColor: "#DEFFE1" }} />
+                    </tr>
+                    <tr>
+                        <Element id='2341421' projects='Architectural drawing' company='Smart Engineering Consultancy' startingDate='29 July 2023' expiryDate='29 July 2023' status='Done' projectHours='200h' workHours='10h 2m' styles={{ color: "#34E045", backgroundColor: "#DEFFE1" }} />
                     </tr>
                     <tr>
                         <Element id='2341421' projects='Architectural drawing' company='Smart Engineering Consultancy' startingDate='29 July 2023' expiryDate='29 July 2023' status='Done' projectHours='200h' workHours='10h 2m' styles={{ color: "#34E045", backgroundColor: "#DEFFE1" }} />
