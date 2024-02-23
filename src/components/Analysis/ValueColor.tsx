6234833fd45ff820954395de1cadd80b1458f9c2
steven <stevensary995@gmail.com>
2024-02-23 10:01:17 +0200

    .

--- src/components/Analysis/ValueColor.tsx
+++ src/components/Analysis/ValueColor.tsx
@@ -11,7 +11,7 @@ type Iprops = {
 const ValueColor = ( { aftColor , name , val , styleName } : Iprops ) => {
   return (
     <>
-    <p style={styleName} className={`relative after:absolute after:bg-[${aftColor === 'design' ? "#343C6A" : aftColor === 'managment' ? "#A1E3CB" : aftColor === 'hr' ? '#7694BA' : aftColor === 'other' ? '#1814F3' : aftColor === 'expand' ? '#7694BA' : aftColor === 'profit' ? '#343C6A' : '' }] after:top-[50%] after:translate-y-[-50%] after:w-[6px] after:h-[6px] after:rounded-full after:left-[-12px] w-[100px]`}>{name}</p>
+    <p style={styleName} className={`relative after:absolute  ${aftColor === 'design' ? "after:bg-[#343C6A]" : aftColor === 'managment' ? "after:bg-[#A1E3CB]" : aftColor === 'hr' ? 'after:bg-[#7694BA]' : aftColor === 'other' ? 'after:bg-[#1814F3]' : aftColor === 'expand' ? 'after:bg-[#7694BA]' : aftColor === 'profit' ? 'after:bg-[#343C6A]' : '' } after:top-[50%] after:translate-y-[-50%] after:w-[6px] after:h-[6px] after:rounded-full after:left-[-12px] w-[100px]`}>{name}</p>
     <p>{val}</p>
     </>
   )
