806ea14f36edf615ac3741dfe8d5d3696f34f03c
Abdelrahman Mostafa <112385425+Abdouumostafa@users.noreply.github.com>
2024-03-07 10:04:00 +0200

    Update formatMoney.ts

--- src/utils/formatMoney.ts
+++ src/utils/formatMoney.ts
@@ -1,11 +1,11 @@
 const formatMoney = (amount: number) => {
     amount = Number(amount);
     if (amount >= 1000000000000) {
-        return `${(amount / 1000000000000).toFixed(0)}T`;
+        return `${(amount / 1000000000000).toFixed(2)}T`;
     } else if (amount >= 1000000000) {
-        return `${(amount / 1000000000).toFixed(0)}B`;
+        return `${(amount / 1000000000).toFixed(1)}B`;
     } else if (amount >= 1000000) {
-        return `${(amount / 1000000).toFixed(0)}M`;
+        return `${(amount / 1000000).toFixed(1)}M`;
     } else if (amount >= 1000) {
         return `${(amount / 1000).toFixed(0)}K`;
     } else {
